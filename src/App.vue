6420600b9fccaa1baef8deeb553a85231058e394
Fredrik Löwenhamn <lowet84@gmail.com>
2017-12-18 16:28:11 +0100

    Playing around some

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,11 @@
 <template>
-  <div>
-    <md-app md-waterfall md-mode="fixed">
-      <md-app-toolbar class="md-primary" >
-        <toolbar></toolbar>
-      </md-app-toolbar>
-
-      <md-app-content id="content">
-        <router-view></router-view>
-      </md-app-content>
-
-      <md-app-drawer id="drawer"
-          :md-active.sync="drawerState" :md-fixed="true">
-        <drawer></drawer>
-      </md-app-drawer>
-    </md-app>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import Toolbar from '@/components/Toolbar'
-import Drawer from '@/components/Drawer'
-import { mapState, mapMutations } from 'vuex'
+
 export default {
-  name: 'app',
-  components: {
-    toolbar: Toolbar,
-    drawer: Drawer
-  },
-  computed: {
-    ...mapState(['showDrawer']),
-    drawerState: {
-      get: function () { return this.showDrawer },
-      set: function (val) { this.setDrawer(val) }
-    }
-  },
-  methods: {
-    ...mapMutations(['setDrawer'])
-  }
+  name: 'app'
 }
 </script>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <md-button class="md-raised md-primary">Primary</md-button>
-    <md-button class="md-raised md-accent">Accent</md-button>
-  </div>
+  <md-app md-waterfall md-mode="flexible">
+    <md-app-toolbar class="md-large md-primary">
+      <div class="md-toolbar-row">
+        <div class="md-toolbar-section-start">
+          <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+            <md-icon>menu</md-icon>
+          </md-button>
+        </div>
+
+        <div class="md-toolbar-section-end">
+          <md-button class="md-icon-button">
+            <md-icon>more_vert</md-icon>
+          </md-button>
+        </div>
+      </div>
+
+      <div class="md-toolbar-row md-toolbar-offset">
+        <span class="md-display-1">My Title</span>
+      </div>
+    </md-app-toolbar>
+
+    <md-app-content id="content">
+      <div class="home">
+        <md-button class="md-raised md-primary">Primary</md-button>
+        <md-button class="md-raised md-accent">Accent</md-button>
+      </div>
+    </md-app-content>
+
+    <md-app-drawer id="drawer"
+        :md-active.sync="drawerState" :md-fixed="true">
+      <drawer></drawer>
+    </md-app-drawer>
+  </md-app>
 </template>
 
 <script>
+import Drawer from '@/components/Drawer'
+import { mapState, mapMutations } from 'vuex'
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    drawer: Drawer
+  },
+  computed: {
+    ...mapState(['showDrawer']),
+    drawerState: {
+      get: function () { return this.showDrawer },
+      set: function (val) { this.setDrawer(val) }
+    }
+  },
+  methods: {
+    ...mapMutations(['setDrawer'])
+  }
 }
 </script>
 
 <style>
 .home{
   height: 200vh;
+}
+.md-app-toolbar {
+    height: 400px;
 }
 </style>
