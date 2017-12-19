408b1e915f61f2b37bbcb3c55f79bced0d05101a
Fredrik Löwenhamn <lowet84@gmail.com>
2017-12-19 14:13:10 +0100

    Switched from vue-material to vuetify

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>content</div>
+  <v-container fluid fill-height>
+    <v-layout justify-center align-center>
+      <v-tooltip right>
+        <v-btn icon large target="_blank" slot="activator">
+          <v-icon large>code</v-icon>
+        </v-btn>
+        <span>Source</span>
+      </v-tooltip>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
 
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
-
-Vue.use(VueMaterial)
+Vue.use(Vuetify)
+import('vuetify/dist/vuetify.min.css')
 
 Vue.config.productionTip = false
 
