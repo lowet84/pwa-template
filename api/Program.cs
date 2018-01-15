0e7b2c13534a97f7b8567b764ccde6a2bc20b3cb
Fredrik Löwenhamn <lowet84@gmail.com>
2018-01-15 15:20:30 +0100

    Adding basic server stub

---
+++ api/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace api
+{
+    public class Program
+    {
+        public const string DatabaseName = "listentoo";
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .UseUrls("http://*:7000")
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ api/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQlRethinkDbLibrary.Database;
+using GraphQlRethinkDbLibrary.Handlers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace api
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE") ?? "localhost";
+            BaseHandler.Setup(
+                app,
+                env,
+                new DatabaseName(Program.DatabaseName),
+                new DatabaseUrl(databaseUrl));
+        }
+    }
+}
